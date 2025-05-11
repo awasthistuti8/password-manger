7df702cd002fe92fdb67235dbc623c622816362d
awasthistuti8 <123956066+awasthistuti8@users.noreply.github.com>
2025-05-11 23:02:59 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full'>
+            <div className="logo font-bold text-white text-2xl">
+
+                <span className="text-fuchsia-500">&lt;</span>
+                Pass
+                <span className="text-fuchsia-500">Man/&gt;</span>
+            </div>
+            <div className='flex justify-center items-center'>
+                Created By Yash Vats
+            </div>
+        </div>
+    )
+}
+
+export default Footer
